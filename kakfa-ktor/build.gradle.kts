d3f87b0cfa73aff7df377b361b630f68b9c9fb9c
Naveed Kamran <3164522+naveedkamran@users.noreply.github.com>
2024-12-12 11:22:32 +0100

    Empty ktor application

---
+++ kakfa-ktor/build.gradle.kts
@@ -0,0 +1,30 @@
+
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.ktor)
+}
+
+group = "com.naveedkamran.examples"
+version = "0.0.1"
+
+application {
+    mainClass.set("io.ktor.server.netty.EngineMain")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+    maven { url = uri("https://packages.confluent.io/maven/") }
+}
+
+dependencies {
+    implementation(libs.ktor.server.kafka)
+    implementation(libs.ktor.server.core)
+    implementation(libs.ktor.server.netty)
+    implementation(libs.logback.classic)
+    implementation(libs.ktor.server.config.yaml)
+    testImplementation(libs.ktor.server.test.host)
+    testImplementation(libs.kotlin.test.junit)
+}
